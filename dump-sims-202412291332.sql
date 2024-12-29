2658b1127b33cedc9ac93651490c72ba5afdd7ee
Fioreza Radhin Naufal <fioreza1902@gmail.com>
2024-12-29 13:34:57 +0700

    feat: new database dll

--- dump-sims-202412291332.sql
+++ dump-sims-202412291332.sql
@@ -31,7 +31,7 @@ CREATE TABLE `banners` (
   `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `banner_name` (`banner_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `banners` (
 
 LOCK TABLES `banners` WRITE;
 /*!40000 ALTER TABLE `banners` DISABLE KEYS */;
-INSERT INTO `banners` VALUES (8,'Banner 1','https://nutech-integrasi.app/dummy.jpg','Lerem Ipsum Dolor sit amet','2024-12-27 10:01:39','2024-12-27 10:01:39'),(9,'Banner 2','https://nutech-integrasi.app/dummy.jpg','Lerem Ipsum Dolor sit amet','2024-12-27 10:01:40','2024-12-27 10:01:40'),(10,'Banner 3','https://nutech-integrasi.app/dummy.jpg','Lerem Ipsum Dolor sit amet','2024-12-27 10:01:40','2024-12-27 10:01:40'),(11,'Banner 4','https://nutech-integrasi.app/dummy.jpg','Lerem Ipsum Dolor sit amet','2024-12-27 10:01:40','2024-12-27 10:01:40'),(12,'Banner 5','https://nutech-integrasi.app/dummy.jpg','Lerem Ipsum Dolor sit amet','2024-12-27 10:01:40','2024-12-27 10:01:40'),(13,'Banner 6','https://nutech-integrasi.app/dummy.jpg','Lerem Ipsum Dolor sit amet','2024-12-27 10:01:40','2024-12-27 10:01:40');
+INSERT INTO `banners` VALUES (1,'Banner 1','https://nutech-integrasi.app/dummy.jpg','Lerem Ipsum Dolor sit amet','2024-12-29 06:01:14','2024-12-29 06:01:14'),(2,'Banner 2','https://nutech-integrasi.app/dummy.jpg','Lerem Ipsum Dolor sit amet','2024-12-29 06:01:14','2024-12-29 06:01:14'),(3,'Banner 3','https://nutech-integrasi.app/dummy.jpg','Lerem Ipsum Dolor sit amet','2024-12-29 06:01:14','2024-12-29 06:01:14'),(4,'Banner 4','https://nutech-integrasi.app/dummy.jpg','Lerem Ipsum Dolor sit amet','2024-12-29 06:01:15','2024-12-29 06:01:15'),(5,'Banner 5','https://nutech-integrasi.app/dummy.jpg','Lerem Ipsum Dolor sit amet','2024-12-29 06:01:15','2024-12-29 06:01:15'),(6,'Banner 6','https://nutech-integrasi.app/dummy.jpg','Lerem Ipsum Dolor sit amet','2024-12-29 06:01:15','2024-12-29 06:01:15');
 /*!40000 ALTER TABLE `banners` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -61,7 +61,7 @@ CREATE TABLE `services` (
   `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `service_code` (`service_code`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +70,7 @@ CREATE TABLE `services` (
 
 LOCK TABLES `services` WRITE;
 /*!40000 ALTER TABLE `services` DISABLE KEYS */;
-INSERT INTO `services` VALUES (13,'PAJAK','Pajak PBB','https://nutech-integrasi.app/dummy.jpg',40000,'2024-12-27 10:01:40','2024-12-27 10:01:40'),(14,'PLN','Listrik','https://nutech-integrasi.app/dummy.jpg',10000,'2024-12-27 10:01:40','2024-12-27 10:01:40'),(15,'PDAM','PDAM Berlangganan','https://nutech-integrasi.app/dummy.jpg',40000,'2024-12-27 10:01:40','2024-12-27 10:01:40'),(16,'PULSA','Pulsa','https://nutech-integrasi.app/dummy.jpg',40000,'2024-12-27 10:01:40','2024-12-27 10:01:40'),(17,'PGN','PGN Berlangganan','https://nutech-integrasi.app/dummy.jpg',50000,'2024-12-27 10:01:40','2024-12-27 10:01:40'),(18,'MUSIK','Musik Berlangganan','https://nutech-integrasi.app/dummy.jpg',50000,'2024-12-27 10:01:40','2024-12-27 10:01:40'),(19,'TV','TV Berlangganan','https://nutech-integrasi.app/dummy.jpg',50000,'2024-12-27 10:01:40','2024-12-27 10:01:40'),(20,'PAKET_DATA','Paket data','https://nutech-integrasi.app/dummy.jpg',50000,'2024-12-27 10:01:40','2024-12-27 10:01:40'),(21,'VOUCHER_GAME','Voucher Game','https://nutech-integrasi.app/dummy.jpg',100000,'2024-12-27 10:01:40','2024-12-27 10:01:40'),(22,'VOUCHER_MAKANAN','Voucher Makanan','https://nutech-integrasi.app/dummy.jpg',100000,'2024-12-27 10:01:41','2024-12-27 10:01:41'),(23,'QURBAN','Qurban','https://nutech-integrasi.app/dummy.jpg',200000,'2024-12-27 10:01:41','2024-12-27 10:01:41'),(24,'ZAKAT','Zakat','https://nutech-integrasi.app/dummy.jpg',300000,'2024-12-27 10:01:41','2024-12-27 10:01:41');
+INSERT INTO `services` VALUES (1,'PAJAK','Pajak PBB','https://nutech-integrasi.app/dummy.jpg',40000,'2024-12-29 06:01:15','2024-12-29 06:01:15'),(2,'PLN','Listrik','https://nutech-integrasi.app/dummy.jpg',10000,'2024-12-29 06:01:15','2024-12-29 06:01:15'),(3,'PDAM','PDAM Berlangganan','https://nutech-integrasi.app/dummy.jpg',40000,'2024-12-29 06:01:15','2024-12-29 06:01:15'),(4,'PULSA','Pulsa','https://nutech-integrasi.app/dummy.jpg',40000,'2024-12-29 06:01:15','2024-12-29 06:01:15'),(5,'PGN','PGN Berlangganan','https://nutech-integrasi.app/dummy.jpg',50000,'2024-12-29 06:01:15','2024-12-29 06:01:15'),(6,'MUSIK','Musik Berlangganan','https://nutech-integrasi.app/dummy.jpg',50000,'2024-12-29 06:01:15','2024-12-29 06:01:15'),(7,'TV','TV Berlangganan','https://nutech-integrasi.app/dummy.jpg',50000,'2024-12-29 06:01:15','2024-12-29 06:01:15'),(8,'PAKET_DATA','Paket data','https://nutech-integrasi.app/dummy.jpg',50000,'2024-12-29 06:01:15','2024-12-29 06:01:15'),(9,'VOUCHER_GAME','Voucher Game','https://nutech-integrasi.app/dummy.jpg',100000,'2024-12-29 06:01:15','2024-12-29 06:01:15'),(10,'VOUCHER_MAKANAN','Voucher Makanan','https://nutech-integrasi.app/dummy.jpg',100000,'2024-12-29 06:01:15','2024-12-29 06:01:15'),(11,'QURBAN','Qurban','https://nutech-integrasi.app/dummy.jpg',200000,'2024-12-29 06:01:15','2024-12-29 06:01:15'),(12,'ZAKAT','Zakat','https://nutech-integrasi.app/dummy.jpg',300000,'2024-12-29 06:01:15','2024-12-29 06:01:15');
 /*!40000 ALTER TABLE `services` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +84,7 @@ DROP TABLE IF EXISTS `transactions`;
 CREATE TABLE `transactions` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
-  `service_code` varchar(255) NOT NULL,
+  `service_code` varchar(255) DEFAULT NULL,
   `description` varchar(100) NOT NULL,
   `transaction_type` enum('TOPUP','PAYMENT') NOT NULL,
   `total_amount` int NOT NULL,
@@ -96,7 +96,7 @@ CREATE TABLE `transactions` (
   KEY `service_code` (`service_code`),
   CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`service_code`) REFERENCES `services` (`service_code`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,6 +105,7 @@ CREATE TABLE `transactions` (
 
 LOCK TABLES `transactions` WRITE;
 /*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (7,1,NULL,'Top Up balance','TOPUP',40000,'INV-1735452603374-1','2024-12-29 06:10:03','2024-12-29 06:10:03');
 /*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -136,7 +137,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'fiorezarn@gmail.com','fioreza','naufal','$2b$10$2BFN.d7WFQ349ZKl8AcGouLohkvEVXkuVdzGipRcOTM06l9BS2pfO',NULL,0,'2024-12-27 10:11:36','2024-12-27 10:11:36');
+INSERT INTO `users` VALUES (1,'fiorezarn@gmail.com','dianti','putri','$2b$10$X4Dzymt7Ra/DHymlowLlSOOcrLjPNVjlivTSXU/Cj28N/xi2tfXb6','1735452123759.png',40000,'2024-12-29 06:01:40','2024-12-29 06:12:07');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -153,4 +154,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-28 15:50:55
+-- Dump completed on 2024-12-29 13:32:59
